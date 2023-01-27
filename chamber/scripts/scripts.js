f3e2f566b7a7c25282befd7a30a0d9cccccf718b
Humpherys-Ross <ross.humpherys@gmail.com>
2023-01-27 11:56:25 -0500

    updating navigation bar

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,14 @@
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
 const copyrightyear = document.querySelector("#copyrightyear");
 const firstname = "Ross";
 const lastname = "Humpherys";
